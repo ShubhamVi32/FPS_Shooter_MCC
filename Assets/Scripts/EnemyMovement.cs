bc2e6f3b9108f7ec4240d46e998dfef99fb81113
Shubham <67814857+ShubhamVi32@users.noreply.github.com>
2024-08-18 08:30:08 +0530

    assets

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyMovement : MonoBehaviour
 {
 
+    public Image HealthBar;
+    public int healthValue;
+    private int currentHealth;
 
     private GameObject player;
     public float chaseTime;
@@ -12,6 +16,7 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentHealth = healthValue;
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -23,12 +28,27 @@ public class EnemyMovement : MonoBehaviour
 
     void Chasing()
     {
-        if(player != null)
+        if (player != null)
         {
-            this.transform.position = Vector3.MoveTowards(transform.position, 
-                player.transform.position, chaseTime * Time.deltaTime);
+            if (Vector3.Distance(player.transform.position, this.transform.position) > 2)
+            {
+                this.transform.position = Vector3.MoveTowards(transform.position,
+                        player.transform.position, chaseTime * Time.deltaTime);
+            }
+           
+
         }
     }
 
+    public void HealthManage(int damage)
+    {
+        currentHealth -= damage;
+        var valueForHealthBar = (float)currentHealth / (float)healthValue;
+        HealthBar.fillAmount = valueForHealthBar;
+        if(currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
 
+    }
 }
